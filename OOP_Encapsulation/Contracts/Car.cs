436f3cb41f2c16458a4e705ea51b17f609ab53fa
yigitcanolmez <yigitcanolmez@outlook.com>
2024-07-29 20:07:09 +0300

    Add project files.

---
+++ OOP_Encapsulation/Contracts/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Encapsulation.Contracts
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public int Year { get; set; }
+
+    }
+
+    public class Car2
+    {
+        public Car2(string brand, int year)
+        {
+            Brand = brand;
+            Year = year;
+        }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class Car3
+    {
+        private string _brand;
+
+        private int _year;
+        public Car3(string brand, int year)
+        {
+            _brand = brand == " " ? "BMW" : brand;
+            _year = year < 2000 ? 2005 : year;
+        }
+
+        public string GetBrand() => _brand;
+        public int GetYear() => _year;
+    }
+
+    public class Car4
+    {
+        private string _brand;
+
+        private int _year;
+        public Car4()
+        {
+
+        }
+        public Car4(string brand, int year)
+        {
+            Brand = brand;
+            Year = year;
+        }
+        public string Brand
+        {
+            get => _brand;
+            set
+            {
+                _brand = value == " " ? "BMW" : value;
+            }
+        }
+        public int Year
+        {
+            get => _year;
+            set
+            {
+                _year = value < 2000 ? 2005 : value;
+            }
+        }
+    }
+}
---
+++ OOP_Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using OOP_Encapsulation.Contracts;
+
+Car car = new Car
+{
+    Brand = "BMW",
+    Year = 1994
+};
+
+Car2 car2 = new Car2("BMW", 1995);
+
+car2.Year = -1440;
+
+Car3 car3 = new Car3(" ", 1992);
+
+car3.GetYear();
+
+car3.GetBrand();
+
+
+Car4 car4 = new Car4();
+
+car4.Year = 1994;
+car4.Brand = " ";
+
+Car4 car4_1 = new Car4("BMW", 1994);
