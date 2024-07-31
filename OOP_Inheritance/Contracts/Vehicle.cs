c7a43c309acb0b5627da849c97c325d51f599772
yigitcanolmez <yigitcanolmez@outlook.com>
2024-07-31 22:13:26 +0300

    added inheritance with dispose pattern %40

---
+++ OOP_Inheritance/Contracts/Vehicle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Inheritance.Contracts
+{
+    public class Vehicle
+    {
+        private string _model { get; set; }
+        private int _date { get; set; }
+        private bool _isSecondHand { get; set; }
+        public Vehicle(string model, int date, bool isSecondHand)
+        {
+            _model = model;
+            _date = date;
+            _isSecondHand = isSecondHand;
+        }
+
+        public string GetVehicleModel() => _model;
+        public int GetVehicleDate() => _date;
+        public bool GetVehicleIsSecondHand() => _isSecondHand;
+
+    }
+    public class Car : Vehicle, IDisposable
+    {
+        private char[] _segments = { 'A', 'B', 'C', 'D', 'E' };
+
+        private char _sportSegment;
+        private bool disposedValue;
+
+        public Car(char sportSegment, string model, int date, bool isSecondHand) : base(model, date, isSecondHand)
+        {
+            SportSegment = sportSegment;
+        }
+
+        public char SportSegment
+        {
+            get => _sportSegment;
+            set
+            {
+                _sportSegment = _segments.Any(p => p.Equals(value)) ? value : 'C';
+            }
+        }
+
+
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~Car()
+        {
+            Console.WriteLine("finalizer çalıştı.");
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ OOP_Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using OOP_Inheritance.Contracts;
+
+Vehicle vehicle = new Car('D', "CUPRA", 2002, false);
+
+vehicle.GetVehicleDate();
+vehicle.GetVehicleIsSecondHand();
+
+
+Car car = new Car('D', "CUPRA", 2002, false);
+
+car.GetVehicleDate();
+
+car.Dispose();
+Console.WriteLine();
