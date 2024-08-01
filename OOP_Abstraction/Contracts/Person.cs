f6a1bea0f95a9fadb4d0b73d8ebd961de6d66756
yigitcanolmez <yigitcanolmez@outlook.com>
2024-08-01 21:16:47 +0300

    added abstact class and methods

---
+++ OOP_Abstraction/Contracts/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace OOP_Abstraction.Contracts
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int BirthDate { get; set; }
+
+        public abstract void Save();
+        public abstract void Delete();
+    }
+    public class Student : Person
+    {
+        public int Grade { get; set; }
+        public override void Save()
+        {
+            Console.WriteLine($"{nameof(Student)} saved");
+        }
+
+        public override void Delete()
+        {
+            Console.WriteLine($"{nameof(Student)} deleted");
+        }
+    }
+    public class Teacher : Person
+    {
+        public string Department { get; set; }
+        public override void Save()
+        {
+            Console.WriteLine($"{nameof(Teacher)} saved");
+        }
+
+        public override void Delete()
+        {
+            Console.WriteLine($"{nameof(Teacher)} deleted");
+        }
+    }
+
+}
